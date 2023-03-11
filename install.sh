f8c09cdaa94841e90c8006ff6b1121723fa40272
mogoll92 <mogoll92@gmail.com>
2023-03-11 16:17:01 +0200

    Updated scripts for shapshots

--- install.sh
+++ install.sh
@@ -10,11 +10,9 @@ mkdir $HOME/snapshots/
 printGreen "Install docker and docker compose"
 bash <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/docker-install.sh) && sleep 1
 
-
 printGreen "Start Nginx"
 PORT=80
-cd $HOME/cosmos-snapshots || return
-docker run --name snapshots --restart always -v $(pwd)/default.conf:/etc/nginx/conf.d/default.conf \
+docker run --name snapshots --restart always -v $(pwd)/cosmos-snapshots/default.conf:/etc/nginx/conf.d/default.conf \
 -v $(pwd)/snapshots/:/root/ -p $PORT:80 -d nginx
 
 printGreen "Nginx has beet started on port :$PORT"
