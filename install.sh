4fc26560e92d172443a45dc8f0764c28960e242d
mogoll92 <mogoll92@gmail.com>
2023-03-17 16:18:50 +0200

    Updated scripts for shapshots

--- install.sh
+++ install.sh
@@ -11,7 +11,9 @@ printGreen "Install docker and docker compose"
 bash <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/docker-install.sh) && sleep 1
 
 printGreen "Start Nginx"
-PORT=80
+
+read -r -p "Enter port for nginx to expose (default 80): " PORT
+PORT=${PORT:-80}
 docker run --name snapshots --restart always -v $HOME/cosmos-snapshots/default.conf:/etc/nginx/conf.d/default.conf \
 -v $HOME/snapshots/:/root/ -p $PORT:80 -d nginx
 
